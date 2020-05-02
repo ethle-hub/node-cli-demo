7d61ddd7af7b06d763dd05d60303e2ea4a9a6681
ele <ele@mtidispatch.com>
2020-05-02 17:38:12 +1000

    complete stage: copy priject files->git->install dependencies

--- src/main.js
+++ src/main.js
@@ -4,75 +4,75 @@ import ncp from 'ncp';              // recursive copying of the files
 import path from 'path';
 import { promisify } from 'util';
 import Listr from 'listr';
+import execa from 'execa';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
 
+// copy the files into the target directory using ncp
 async function copyTemplateFiles(options) {
- return await copy(options.templateDirectory, options.targetDirectory, {
-   clobber: false,
- });
+  return await copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+// execute git init command from target directory
+async function initGit(options) {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory,
+  });
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize git'));
+  }
+  return;
 }
 
 export async function createProject(options) {
- options = {
-   ...options,
-   targetDirectory: options.targetDirectory || process.cwd(),
- };
+  options = {
+    ...options,
+    targetDirectory: options.targetDirectory || process.cwd(),
+  };
 
- const currentFileUrl = __dirname; // as 'import.meta.url' has extra 'C:\' at beginning
- const templateDir = path.resolve(
-   new URL(currentFileUrl).pathname,
-   '../templates',
-   options.template.toLowerCase()
- ); 
- options.templateDirectory = templateDir;
- 
- // 1) checking read access
- 
-console.log(`%s Checking read acces path=${templateDir}`, chalk.cyan('INFO'));   
-await access(templateDir, fs.constants.R_OK)
-  .catch ( error => {     
-   console.error(`%s Invalid template name at ${templateDir}`, chalk.red.bold('ERROR'));
-   process.exit(1);
-  });
+  const currentFileUrl = __dirname; // as 'import.meta.url' has extra 'C:\' at beginning
+  const templateDir = path.resolve(
+    new URL(currentFileUrl).pathname,
+    '../templates',
+    options.template.toLowerCase()
+  );
+  options.templateDirectory = templateDir;
 
- // 2) then copy the files into the target directory using ncp
- console.log(`%s Copy project files from=${options.templateDirectory} to=${options.targetDirectory}`, chalk.cyan('INFO'));
+  // 1) checking read access
+  console.log(`%s Checking read acces path=${templateDir}`, chalk.cyan('INFO'));
+  await access(templateDir, fs.constants.R_OK);
 
+  console.log(`%s Copy project files from=${options.templateDirectory} to=${options.targetDirectory}`, chalk.cyan('INFO'));
 
-//  await copyTemplateFiles(options)
-//   .catch(error => { 
-//     console.log(`%s ${error.message}`, chalk.red.bold('ERROR')); 
-//     process.exit(1);
-//   });
-
-  
+  // `listr` which let's us specify a list of tasks and gives the user a neat progress overview
   const tasks = new Listr([
     {
       title: 'Copy project files',
       task: () => copyTemplateFiles(options),
     },
-    // {
-    //   title: 'Initialize git',
-    //   task: () => initGit(options),
-    //   enabled: () => options.git,
-    // },
-    // {
-    //   title: 'Install dependencies',
-    //   task: () =>
-    //     projectInstall({
-    //       cwd: options.targetDirectory,
-    //     }),
-    //   skip: () =>
-    //     !options.runInstall
-    //       ? 'Pass --install to automatically install dependencies'
-    //       : undefined,
-    // },
+    {
+      title: 'Initialize git',
+      task: () => initGit(options),
+      enabled: () => options.git,
+    },
+    {
+      title: 'Install dependencies',
+      task: () => projectInstall({
+          cwd: options.targetDirectory,
+        }),
+      skip: () =>
+        !options.runInstall
+          ? 'Pass --install to automatically install dependencies'
+          : undefined,
+    },
   ]);
- 
+
   await tasks.run();
 
- console.log('%s Project ready', chalk.green.bold('DONE'));
- return true;
+  console.log('%s Project ready', chalk.green.bold('DONE'));
+  return true;
 }
